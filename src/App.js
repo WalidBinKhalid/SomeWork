f3c1685555dc9f9d11a9957c5171c7f460f070ae
WalidBinKhalid <khalidwaleed875@gmail.com>
2017-11-05 22:05:50 +0500

    initial directory structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ShoppingList extends React.Component{
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default App;
